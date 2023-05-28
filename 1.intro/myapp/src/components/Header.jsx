c8c2ded43bda7efb0b694475830acbf456f6b71f
Harsh <goyalh4164@gmail.com>
2023-05-28 16:00:43 +0530

    prevented page reloading by the use of map

--- 1.intro/myapp/src/components/Header.jsx
+++ 1.intro/myapp/src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import "../styles/Header.css"
+// to prevent page reloading we will use the 'Link' tag inplace of anchor tag
+import { Link } from 'react-router-dom'
+
 const Header = () => {
   return (
     <nav>
       <ul>
-        <li><a href="/"> Home </a></li>
-        <li><a href="/about"> About </a></li>
-        <li><a href="/contact"> Contact </a></li>
+        <li><Link to="/"> Home </Link></li>
+        <li><Link to="/about"> About </Link></li>
+        <li><Link to="/contact"> Contact </Link></li>
       </ul>
     </nav>
   )
