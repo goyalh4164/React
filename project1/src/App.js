ffa9d1d0c7e733e9ff281daba529d8b583f004f9
Harsh <goyalh4164@gmail.com>
2023-05-28 17:24:06 +0530

    added styling to the header

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,8 +1,13 @@
 import {BrowserRouter as Router ,Route ,Routes} from "react-router-dom"
+import Header from "./components/Header";
 
+// styling imports
+import "./styles/app.scss"
+import "./styles/header.scss"
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<div>hello</div>}/>
       </Routes>
