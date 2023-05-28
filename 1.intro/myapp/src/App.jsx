fd83bb0ee23264f5808f4ceeb1c3cbb031b3cd9e
Harsh <goyalh4164@gmail.com>
2023-05-28 14:39:00 +0530

    starting of a new fresh project to learn styling with the react

--- 1.intro/myapp/src/App.jsx
+++ 1.intro/myapp/src/App.jsx
@@ -1,21 +1,11 @@
-import MyHeading ,{MyHeading1 as CustomHeading, MyHeading2} from "./components/MyHeading";
-
-
-function App(){
-    return(
-        <>
-        <div>
-            <MyHeading name="Harsh" department="computer science" price="200"/>
-            <h1>Hello</h1>
-            <p>this is a paragraph</p>
-            <CustomHeading/>
-            <MyHeading2/>
-        </div>
-        <div>
-            This is a second div
-        </div>
-        </>
-    );
-}
+import React from "react";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 export default App;
