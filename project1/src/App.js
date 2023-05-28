2cd9043c19c7e040131b93e600914dede1b1cacd
Harsh <goyalh4164@gmail.com>
2023-05-28 18:56:47 +0530

    Home component done

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,15 +1,17 @@
 import {BrowserRouter as Router ,Route ,Routes} from "react-router-dom"
 import Header from "./components/Header";
-
+import Home from "./components/Home";
 // styling imports
 import "./styles/app.scss"
 import "./styles/header.scss"
+import "./styles/home.scss"
+
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route path="/" element={<div>hello</div>}/>
+        <Route path="/" element={<Home/>}/>
       </Routes>
     </Router>
   );
