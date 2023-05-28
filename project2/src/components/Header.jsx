bb04f3e3d33790365da7022d8fb6fe925ea45674
Harsh <goyalh4164@gmail.com>
2023-05-28 21:28:53 +0530

    paused react 2 project

--- project2/src/components/Header.jsx
+++ project2/src/components/Header.jsx
@@ -14,7 +14,16 @@ import { BiMenuAltLeft } from 'react-icons/bi';
 const Header = () => {
   return (
   <>
-  <Button>
+  <Button
+  pos={'fixed'}
+  top={"4"}
+  left={"4"}
+  colorScheme='purple'
+  p={"0"}
+  w={"10"}
+  h={"10"}
+  borderRadius={'full'}
+  >
     <BiMenuAltLeft size={'20'} />
   </Button>
   </>
