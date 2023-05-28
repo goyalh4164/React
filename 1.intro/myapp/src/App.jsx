1a84c44509610c2c5e55ff1ca074ea0c8ea983e9
Harsh <goyalh4164@gmail.com>
2023-05-28 15:37:25 +0530

    Basic routing using the react-router-dom

--- 1.intro/myapp/src/App.jsx
+++ 1.intro/myapp/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import Header from "./components/Header";
-
 import "./styles/App.css"
+import {BrowserRouter as Router , Routes ,Route} from "react-router-dom"
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
 const App = () => {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+    </Routes>
+  </Router>
 };
 
 export default App;
