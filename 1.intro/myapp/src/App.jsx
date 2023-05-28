413432081264508f13b7a8a08363c6076a6e26cb
Harsh <goyalh4164@gmail.com>
2023-05-28 15:44:26 +0530

    multiple page routing

--- 1.intro/myapp/src/App.jsx
+++ 1.intro/myapp/src/App.jsx
@@ -4,9 +4,11 @@ import {BrowserRouter as Router , Routes ,Route} from "react-router-dom"
 import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 const App = () => {
   return <Router>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
--- 1.intro/myapp/src/components/Header.jsx
+++ 1.intro/myapp/src/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import "../styles/Header.css"
 const Header = () => {
   return (
-    <nav> This is a Navbar</nav>
+    <nav>
+      <ul>
+        <li><a href="/"> Home </a></li>
+        <li><a href="/about"> About </a></li>
+        <li><a href="/contact"> Contact </a></li>
+      </ul>
+    </nav>
   )
 }
 
